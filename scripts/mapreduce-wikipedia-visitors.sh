a1a7a60bf23c144d714fc3c5d93bc7aa3ee0fe3a
Lukas Trumm <lukas.trumm@centrum.cz>
2015-10-15 22:46:34 +0200

    Rewrite bash script to process pagecounts through pipes

--- scripts/mapreduce-wikipedia-visitors.sh
+++ scripts/mapreduce-wikipedia-visitors.sh
@@ -1,2 +1,14 @@
-cat pc1 pc2 | gawk '$1=="cs" && length($2)<100 {a[$2]+=$3} END{for (i in a) print i,a[i]}' | sort -r -n -k 2 | he
-ad -50 | urldecode > results
+# ungzip all input files to stdout
+gunzip -c ../data/${1}-data/pagecounts*.gz |
+# sum all lines starting with "cs" and not too long
+# finally print the associative array
+awk '
+	$1=="cs" && length($2)<100 {a[$2]+=$3}
+	END { for (i in a) print i,a[i] }
+' |
+# sort lines by second column (number values) descendantly
+sort -r -n -k 2 |
+# top 20 is enough
+head -20 |
+# decode url encoding
+python -c "import sys, urllib as ul; print ul.unquote_plus(sys.stdin.read())"
