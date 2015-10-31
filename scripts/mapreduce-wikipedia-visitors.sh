22750d12e57ec2e82f9881bfb8476a0ecb6f4148
Lukas Trumm <lukas.trumm@centrum.cz>
2015-10-31 09:29:43 +0100

    Improve speed of bash script

--- scripts/mapreduce-wikipedia-visitors.sh
+++ scripts/mapreduce-wikipedia-visitors.sh
@@ -33,7 +33,8 @@ do
 		END { for (i in a) print i,a[i] }
 	' |
 	# decode url encoding
-	python -c "import sys, urllib as ul; print ul.unquote_plus(sys.stdin.read())" \
+	# python -c "import sys, urllib as ul; print ul.unquote_plus(sys.stdin.read())" |
+	sort \
 	> $SUM_DIR/$key
 done
 
@@ -53,23 +54,25 @@ stopwatch_totals=$(date +%s)
 printf "%-25s %s\n" "Totals calculated in " $(expr $stopwatch_totals - $stopwatch_sums)
 
 # Fill files with chronological page counts for top pages
-declare -a top_pages=$(awk '{print $1}' $TOTAL_DIR/total | sort)
+declare -a top_pages=($(awk '{print $1}' $TOTAL_DIR/total | sort))
 for day_file in $SUM_DIR/* # for every day
 do
+	top_pages_counter=1
+	day_number=${day_file: -2}
+
 	while read line # for every page and its sum of its counts in the day
 	do
-		page_name=$(echo $line | awk '{print $1}')
-		page_count=$(echo $line | awk '{print $2}')
-		for top_page in ${top_pages[@]}
-		do
-			# test if the page is in top pages
-			if [ "$top_page" == "$page_name" ] 
-			then
-				day_number=${day_file: -2}
-				# and append the page count to appropriate file
-				echo $day_number" "$page_count >> $PAGES_DIR/$page_name
-			fi
-		done
+		line_array=($line)
+		page_name=${line_array[0]}
+		page_count=${line_array[1]}
+		top_page=${top_pages[$top_pages_counter]}
+		# test if the page is in top pages
+		if [ "$top_page" = "$page_name" ] 
+		then
+			# append the page count to appropriate file
+			echo $day_number" "$page_count >> $PAGES_DIR/$page_name
+			((top_pages_counter+=1)) # move to next top page
+		fi
 	done < $day_file
 done
 
